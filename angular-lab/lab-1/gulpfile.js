0a694d977d722c246d8714b481c1aada90bd6ad8
perksadam <perksadam@gmail.com>
2016-06-15 20:33:15 -0400

    adding gulpfile

---
+++ angular-lab/lab-1/gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+// Static server
+gulp.task('run', ['test'], function () {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch("./**/*.html").on('change', browserSync.reload);
+    gulp.watch("./**/*.js").on('change', browserSync.reload);
+    gulp.watch("./**/*.css").on('change', browserSync.reload);
+});
+
+gulp.task('test', function () {
+
+});
